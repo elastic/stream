000f151fdc9d96c3c34242709ac5d9b2eb186665
Marius Iversen <marius.iversen@elastic.co>
2022-11-23 14:03:06 +0100

    linting

--- pkg/output/abs/abs.go
+++ pkg/output/abs/abs.go
@@ -17,10 +17,8 @@ func init() {
 }
 
 type Output struct {
-	opts       *output.Options
-	client     *azblob.Client
-	ctx        context.Context
-	cancelFunc func()
+	opts   *output.Options
+	client *azblob.Client
 }
 
 func New(opts *output.Options) (output.Output, error) {
@@ -28,14 +26,13 @@ func New(opts *output.Options) (output.Output, error) {
 		return nil, errors.New("azure blob storage address is required")
 	}
 	connectionString := fmt.Sprintf("DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://%s:%s/devstoreaccount1;", opts.Addr, opts.ABSOptions.Port)
-	ctx, cancel := context.WithCancel(context.Background())
 	serviceClient, _ := azblob.NewClientFromConnectionString(connectionString, nil)
 
-	return &Output{opts: opts, client: serviceClient, ctx: ctx, cancelFunc: cancel}, nil
+	return &Output{opts: opts, client: serviceClient}, nil
 }
 
 func (o *Output) DialContext(ctx context.Context) error {
-	err := o.createContainer()
+	err := o.createContainer(ctx)
 	if err != nil {
 		return err
 	}
@@ -43,20 +40,19 @@ func (o *Output) DialContext(ctx context.Context) error {
 }
 
 func (o *Output) Close() error {
-	o.cancelFunc()
 	return nil
 }
 
 func (o *Output) Write(b []byte) (int, error) {
-	_, err := o.client.UploadBuffer(o.ctx, o.opts.ABSOptions.Container, o.opts.ABSOptions.Blob, b, nil)
+	_, err := o.client.UploadBuffer(context.Background(), o.opts.ABSOptions.Container, o.opts.ABSOptions.Blob, b, nil)
 	if err != nil {
 		return 0, fmt.Errorf("failed to upload file to blob: %w", err)
 	}
 	return len(b), nil
 }
 
-func (o *Output) createContainer() error {
-	_, err := o.client.CreateContainer(o.ctx, o.opts.ABSOptions.Container, nil)
+func (o *Output) createContainer(ctx context.Context) error {
+	_, err := o.client.CreateContainer(ctx, o.opts.ABSOptions.Container, nil)
 	if err != nil {
 		return err
 	}
