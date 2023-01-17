f02e0defb372b466a94176469caf47823529b842
Marius Iversen <marius.iversen@elastic.co>
2023-01-17 12:05:25 +0100

    adding better package name and root commands

--- pkg/output/azureblobstorage/azure_blob_storage.go
+++ pkg/output/azureblobstorage/azure_blob_storage.go
@@ -1,7 +1,7 @@
 // Licensed to Elasticsearch B.V. under one or more agreements.
 // Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
-package azure_blob_storage
+package azureblobstorage
 
 import (
 	"context"
@@ -14,7 +14,7 @@ import (
 )
 
 func init() {
-	output.Register("azure_blob_storage", New)
+	output.Register("azureblobstorage", New)
 }
 
 type Output struct {
