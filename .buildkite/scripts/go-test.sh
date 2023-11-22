c98abb74f4567f52dfcd81f71259200edb86fe6a
Olga Naidjonoka <olga_naidjonoka@epam.com>
2023-11-22 12:56:26 +0200

    added mage to go-test script

--- .buildkite/scripts/go-test.sh
+++ .buildkite/scripts/go-test.sh
@@ -6,4 +6,8 @@ source .buildkite/scripts/common.sh
 
 add_bin_path
 with_go "${GOLANG_VERSION}"
+with_mage
+
+echo "Starting tests"
 gotestsum --format testname --junitfile junit-report.xml -- '-v ./...'
+echo "Tests done!"
--- .buildkite/scripts/lint.sh
+++ .buildkite/scripts/lint.sh
@@ -7,6 +7,8 @@ source .buildkite/scripts/common.sh
 add_bin_path
 with_go "${GOLANG_VERSION}"
 
+echo "Starting lint"
 go mod tidy && git diff --exit-code
 make check-fmt
 go vet
+echo "Lint done!"
