fd04728175770f99e38fb8f9204e69b568e12eda
Andrew Kroh <andrew.kroh@elastic.co>
2024-03-22 22:06:20 -0400

    docker-build: create multi-arch image amd64,arm64 (#84)
    Use docker buildx to produce a multi-arch image that can run natively on linux/amd64 and linux/arm64.
        
    Sanitize the branch name because it is used as a docker tag. Special characters need to be removed.
    
    Closes #77
    
    References
    - https://docs.docker.com/reference/cli/docker/image/tag/
    - https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html

--- .buildkite/scripts/image-util.sh
+++ .buildkite/scripts/image-util.sh
@@ -28,18 +28,30 @@ docker_branch_tag() {
     branch_tag="${branch}"
   fi
 
+  # Sanitize the branch name using parameter expansion.
+  # The tag must be valid ASCII and can contain lowercase and uppercase letters,
+  # digits, underscores, periods, and hyphens. It can't start with a period or
+  # hyphen and must be no longer than 128 characters.
+  branch_tag=${branch_tag//[^a-zA-Z0-9]/_}
+  branch_tag=${branch_tag:0:128}
+
   echo "${image}:${branch_tag}"
 }
 
-build_image() {
-  local commit_tag=$1
-  local base_commit=$2
+build_and_push_image() {
+  local base_commit=$1
+  shift
+  local extra_args=("$@")
 
-  docker build \
-    -t "${commit_tag}" \
+  docker buildx create --platform linux/amd64,linux/arm64 --use
+  docker buildx build \
+    --progress=plain \
+    --platform linux/amd64,linux/arm64 \
+    --push \
     --label BRANCH_NAME="${BUILDKITE_BRANCH}" \
     --label GIT_SHA="${base_commit}" \
     --label GO_VERSION="${GOLANG_VERSION}" \
     --label TIMESTAMP="$(date +%Y-%m-%d_%H:%M)" \
+    ${extra_args[@]} \
     .
 }
