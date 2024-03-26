3f83fd67541e03a4eeabd5ac40509fdcf251e37f
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-03-26 16:51:57 +0200

    fix: utilise docker buildx imagetools to support re-tagging multi-platform images (#87)

--- .buildkite/scripts/retag.sh
+++ .buildkite/scripts/retag.sh
@@ -12,6 +12,4 @@ old_tag=$(docker_commit_tag "${docker_img}" "${base_commit}")
 new_tag="${BUILDKITE_TAG}"
 
 echo ":: Re-tagging image from ${old_tag} to ${new_tag} ::"
-retry 3 docker pull "${old_tag}"
-retry 3 docker tag  "${old_tag}" "${new_tag}"
-retry 3 docker push "${new_tag}"
+retry 3 docker buildx imagetools create -t "${new_tag}" "${old_tag}"
