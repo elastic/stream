1d7f2b8c079ffb067c34f231da716976e5667f6a
Andrew Kroh <andrew.kroh@elastic.co>
2024-04-05 10:25:27 -0400

    It can't start with a period or hyphen

--- .buildkite/scripts/image-util.sh
+++ .buildkite/scripts/image-util.sh
@@ -33,6 +33,7 @@ docker_branch_tag() {
   # digits, underscores, periods, and hyphens. It can't start with a period or
   # hyphen and must be no longer than 128 characters.
   branch_tag=${branch_tag//[^-.a-zA-Z0-9]/_}
+  branch_tag=${branch_tag/#[-.]/_}
   branch_tag=${branch_tag:0:128}
 
   echo "${image}:${branch_tag}"
