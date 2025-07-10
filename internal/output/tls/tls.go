bcb9bb1f95e51e8d6cf26e2ae4d81560807d7690
Andrew Kroh <andrew.kroh@elastic.co>
2025-07-09 20:46:14 -0400

    fix(tls,udp): match package name to dir name

--- internal/output/tls/tls.go
+++ internal/output/tls/tls.go
@@ -2,8 +2,8 @@
 // Elasticsearch B.V. licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-package tcp
 // Package tls provides an output that writes data to a TLS+TCP connection.
+package tls
 
 import (
 	"context"
